90fe7183370a611f480bf698b4ffe303a4a63deb
Vermee81 <hrksb5029@gmail.com>
2016-05-29 17:36:22 +0900

    [modify] Closes #11 Switch twitter accounts when you request
    Switching in 2 twitter accounts when you request to Twitter API

--- libs/tweets/twitter_crawler.py
+++ libs/tweets/twitter_crawler.py
@@ -13,25 +13,28 @@ from libs.fuel import fuel
 ###import ignore_twitter_keys.py
 
 
-def get_tweets(fuel_tank = "", since_id = ""):
 
-    conf = ConfigParser.SafeConfigParser()
-    conf.read('/home/pi/nama_phone_engine/libs/tweets/ignored_twitter_keys.cfg')
-    CK = conf.get('twitterkeys', 'cons_key')
-    CS =  conf.get('twitterkeys', 'cons_sec')
-    AT = conf.get('twitterkeys', 'acc_token')
-    AS = conf.get('twitterkeys', 'acc_sec')
-
-    consumer = oauth.Consumer(key=CK, secret=CS)
-    token = oauth.Token(key=AT, secret=AS)
+def get_tweets(fuel_tank = "", since_id = "", account_name = ""):
 
     ACCOUNT = u"@namaphonebox"
-    COUNT = 100
+    COUNT = 100 ## max100件
 
     query = ACCOUNT.encode('utf-8')
 
     url = 'https://api.twitter.com/1.1/search/tweets.json'
     params = {'q': query, 'since_id': since_id, 'count': COUNT}
+
+    conf = ConfigParser.SafeConfigParser()
+    conf.read('/home/pi/nama_phone_engine/libs/tweets/ignored_twitter_keys.cfg')
+
+    ## ここから配列
+    CK = conf.get(account_name, 'cons_key')
+    CS =  conf.get(account_name, 'cons_sec')
+    AT = conf.get(account_name, 'acc_token')
+    AS = conf.get(account_name, 'acc_sec')
+
+    consumer = oauth.Consumer(key=CK, secret=CS)
+    token = oauth.Token(key=AT, secret=AS)
 
     request = oauth.Request.from_consumer_and_token(consumer, token, http_url=url,parameters=params)
     request.sign_request(oauth.SignatureMethod_HMAC_SHA1(), consumer, token)
@@ -56,16 +59,24 @@ class TwitterThread(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
         self.fuel_tank = fuel.Fuel()
-        self.INTERVAL = 60
+        self.INTERVAL = 30
+        self.TWITTER_ACCOUNTS = ['twitterkeys', 'twitterkeys2']
 
     def run(self):
         max_id = ""
-        max_id = get_tweets()
+        account_index = 0
+        max_id = get_tweets(account_name=self.TWITTER_ACCOUNTS[account_index])
+        counter = 1
         time.sleep(self.INTERVAL)
         while 1:
-            counter = 0
-            max_id = get_tweets(self.fuel_tank, max_id)
+            #次のアカウントへ切り替える
+            account_index = counter % len(self.TWITTER_ACCOUNTS)
+
+            max_id = get_tweets(self.fuel_tank, max_id, self.TWITTER_ACCOUNTS[account_index])
             print "FUEL: " + str(self.fuel_tank.fuel)
+
+            counter = counter + 1
+
             time.sleep(self.INTERVAL)
 
 if __name__ == '__main__':
